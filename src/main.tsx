986e855010c752e71600fddf015b0c85f40ab55e
bobr <paveleagle2005@gmail.com>
2025-11-21 16:20:21 +0300

    feat: add base routing

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/list' element={<App />} />
+        <Route path='*' element={<Navigate to='/list' replace />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
