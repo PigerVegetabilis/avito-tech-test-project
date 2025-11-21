6ac94dd1ee01880d4dc6d7bc5bdb7997808d7185
bobr <paveleagle2005@gmail.com>
2025-11-21 17:03:07 +0300

    chore: add cors dependency and policy

--- src/tech-int3-server/server.js
+++ src/tech-int3-server/server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.static('public'));
-
+app.use(cors());
 app.use('/api/v1', require('./src/routes/v1'));
 
 app.get('/', (req, res) => {
